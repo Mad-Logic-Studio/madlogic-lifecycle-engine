4ebdc4204e29be2e2f5e97a39c91aa7aac0f3c3a
Mad Logic Studio <ops@madlogicstudio.com>
2026-08-10 04:43:49 -0600

    WP1: export lifecycle state and event processing APIs

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,3 +2,5 @@ export * from './types.js';
 export * from './normalize.js';
 export * from './identity.js';
 export * from './event-ledger.js';
+export * from './event-processing.js';
+export * from './lifecycle-state.js';
